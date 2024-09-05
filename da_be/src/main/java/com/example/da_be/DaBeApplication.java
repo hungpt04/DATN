3498707560c815336d56016a4c5e393530330c4b
phamhungg <phamhungg2709@gmail.com>
2024-09-05 14:05:46 +0700

    Init project

---
+++ da_be/src/main/java/com/example/da_be/DaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.da_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaBeApplication.class, args);
+    }
+
+}
